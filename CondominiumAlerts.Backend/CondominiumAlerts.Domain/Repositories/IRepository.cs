34564604ef8107913334eea65b1580fba2f4592c
Adrian Polanco <adferrer0611@gmail.com>
2025-02-01 20:09:41 -0500

    chore: Implementación del patrón Repository

--- CondominiumAlerts.Backend/CondominiumAlerts.Domain/Repositories/IRepository.cs
+++ CondominiumAlerts.Backend/CondominiumAlerts.Domain/Repositories/IRepository.cs
@@ -1,12 +1,19 @@
-﻿using CondominiumAlerts.Domain.Aggregates.Entities;
+﻿using System.Linq.Expressions;
+using CondominiumAlerts.Domain.Aggregates.Entities;
 using CondominiumAlerts.Domain.Aggregates.Interfaces;
 
 namespace CondominiumAlerts.Domain.Repositories;
 
-public interface IRepository<TEntity, TId> where TEntity : class, IEntity<TId>
+public interface IRepository<TEntity> where TEntity : class, IEntity<Guid>
 {
-    Task<List<TEntity>> GetByIdAsync(TId id, CancellationToken cancellationToken = default);
+    Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken, bool readOnly = false, bool ignoreQueryFilters = false, params Expression<Func<TEntity, object>>[] includes);
+
+    Task<List<TEntity>> GetAsync(CancellationToken cancellationToken, Expression<Func<TEntity, bool>>? filter = null,
+        bool readOnly = true, bool ignoreQueryFilters = false, Expression<Func<TEntity, object>>[]? includes = null);
     Task<TEntity> CreateAsync(TEntity entity, CancellationToken cancellationToken = default);
-    Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
-    Task<TEntity> DeleteAsync(TId id, CancellationToken cancellationToken = default);
+    Task<List<TEntity>> BulkInsertAsync(List<TEntity> entities, CancellationToken cancellationToken);
+    Task<TEntity?> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task<List<TEntity>> BulkUpdateAsync(List<TEntity> entities, CancellationToken cancellationToken);
+    Task<TEntity?> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<List<TEntity>> BulkDeleteAsync(List<TEntity> entities, CancellationToken cancellationToken);
 }
