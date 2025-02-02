100aea23c6e2ef9bf14946d866614250aaa6a4a6
Adrian Polanco <adferrer0611@gmail.com>
2025-02-01 22:03:56 -0500

    check: Null Error en RegisterUserValidator.cs

--- CondominiumAlerts.Backend/CondominiumAlerts.Features/Features/Users/Register/RegisterUserValidator.cs
+++ CondominiumAlerts.Backend/CondominiumAlerts.Features/Features/Users/Register/RegisterUserValidator.cs
@@ -1,8 +1,10 @@
 ﻿
 using FluentValidation;
+using PhoneNumbers;
 
 namespace CondominiumAlerts.Features.Commands;
 
+//RegisterUserCommand(string Username, string Email, string Password, string Name, string Lastname, Phone PhoneNumber, Address Address)
 public class RegisterUserValidator : AbstractValidator<RegisterUserCommand>
 {
     public RegisterUserValidator()
@@ -16,5 +18,28 @@ public class RegisterUserValidator : AbstractValidator<RegisterUserCommand>
             .Matches(@"[0-9]").WithMessage("La contraseña debe contener al menos un número.") // Al menos un número
             .Matches(@"[\W_]").WithMessage("La contraseña debe contener al menos un carácter especial.") // Al menos un carácter especial
             .Matches(@"^[a-zA-Z0-9\W_]+$").WithMessage("La contraseña solo puede contener caracteres alfanuméricos y especiales.");
+        RuleFor(u => u.Name).NotEmpty().WithMessage("El nombre de usuario es requerido.");
+        RuleFor(u => u.Lastname).NotEmpty().WithMessage("La nombre de usuario es requerido.");
+        RuleFor(u => u.Lastname).MinimumLength(3).MaximumLength(200).WithMessage("El apellido debe tener entre 3 y 200 caracteres.");
+        RuleFor(u => u.PhoneNumber).NotNull().WithMessage("El phone number es requerido.");
+        // Validación de PhoneNumber
+        RuleFor(u => u.PhoneNumber)
+            .NotNull().WithMessage("El número de teléfono es requerido."); // Verifica que PhoneNumber no sea null
+        RuleFor(u => u.PhoneNumber.Number).Must(p => BeAValidPhoneNumber(p)).WithMessage("El número de teléfono debe ser válido.") // Verifica que el número sea válido
+            .When(u => !string.IsNullOrEmpty(u.PhoneNumber?.Number)); // Solo verifica que sea válido si PhoneNumber existe
+    }
+    
+    private bool BeAValidPhoneNumber(string phoneNumber)
+    {
+        try
+        {
+            var phoneNumberUtil = PhoneNumberUtil.GetInstance();
+            var number = phoneNumberUtil.Parse(phoneNumber, "ZZ"); // "ZZ" permite cualquier país
+            return phoneNumberUtil.IsValidNumber(number);
+        }
+        catch (NumberParseException)
+        {
+            return false;
+        }
     }
 }
