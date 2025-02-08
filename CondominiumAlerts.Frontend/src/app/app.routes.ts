345b9b7ed3da385f730938e3717c39285ee13da7
Luis De Los Santos <dluisaneudy82@gmail.com>
2025-02-07 22:39:45 -0400

    LOGIN PAGE

--- CondominiumAlerts.Frontend/src/app/app.routes.ts
+++ CondominiumAlerts.Frontend/src/app/app.routes.ts
@@ -2,9 +2,24 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: "register",
-    title: "Registrar usuario",
-    loadComponent: () => import('./core/register/pages/register-user-page/register-user-page.component')
-                                        .then(c => c.RegisterUserPageComponent)
-  }
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'register',
+    title: 'Registrar usuario',
+    loadComponent: () =>
+      import(
+        './core/register/pages/register-user-page/register-user-page.component'
+      ).then((c) => c.RegisterUserPageComponent),
+  },
+  {
+    path: 'login',
+    title: 'Iniciar sesión',
+    loadComponent: () =>
+      import('./core/auth/pages/sing-in/sing-in.component').then(
+        (c) => c.SingInComponent
+      ),
+  },
 ];
