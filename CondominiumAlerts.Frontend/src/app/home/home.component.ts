e01562c7b7cdd8608bcedda4052f165de4f05aab
Israel jimenez <sylcredjimenez19@gmail.com>
2025-02-12 13:08:03 -0400

    feat: botones dirigen al login y registro

--- CondominiumAlerts.Frontend/src/app/home/home.component.ts
+++ CondominiumAlerts.Frontend/src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { Toolbar } from 'primeng/toolbar';
 import { NgOptimizedImage } from '@angular/common';
+import { RouterModule, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule, Toolbar, NgOptimizedImage], 
+  imports: [ButtonModule, Toolbar, NgOptimizedImage, RouterModule, RouterLink], 
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
