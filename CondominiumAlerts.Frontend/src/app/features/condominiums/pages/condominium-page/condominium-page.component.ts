3b406d57bd80a53b91d5f21a44bfb7f9e5856ad3
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2025-02-17 19:06:18 -0400

    feat: Add basic frotend scaffolding for condominium

---
+++ CondominiumAlerts.Frontend/src/app/features/condominiums/pages/condominium-page/condominium-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-condominium-page',
+  imports: [],
+  templateUrl: './condominium-page.component.html',
+  styleUrl: './condominium-page.component.css'
+})
+export class CondominiumPageComponent {
+
+}
