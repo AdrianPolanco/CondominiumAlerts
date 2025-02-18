455fac42d2efa19211fa3cd73d8db2be0ba38de3
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2025-02-17 20:10:13 -0400

    feat: Finish up part of the condomium creation

--- CondominiumAlerts.Frontend/src/app/features/condominiums/pages/condominium-page/condominium-page.component.ts
+++ CondominiumAlerts.Frontend/src/app/features/condominiums/pages/condominium-page/condominium-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CondominiumService } from '../../services/condominium.service';
 
 @Component({
   selector: 'app-condominium-page',
@@ -8,4 +10,27 @@ import { Component } from '@angular/core';
 })
 export class CondominiumPageComponent {
 
+  form: FormGroup;
+    constructor(private fb: FormBuilder, private condominiumService: CondominiumService) {
+    this.form = this.fb.group({
+      name: [''],
+      address: [''],
+      imageFile: [null],
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+        this.condominiumService.create(this.form.value);
+    }
+  }
+
+  onFileSelect(event: any) {
+    if (event.files.length > 0) {
+      const file = event.files[0];
+      this.form.patchValue({
+        imageFile: file,
+      });
+    }
+  }
 }
