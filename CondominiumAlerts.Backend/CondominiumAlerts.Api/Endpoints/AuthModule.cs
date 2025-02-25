85060e7765dcd4d5e50b22602f45e7c50731f5b4
Adrian Polanco <adferrer0611@gmail.com>
2025-02-24 19:56:54 -0500

    feat: Agregado Username a los responses de GetUserDataResponse.cs y cambiado de LastName a Lastname

--- CondominiumAlerts.Backend/CondominiumAlerts.Api/Endpoints/AuthModule.cs
+++ CondominiumAlerts.Backend/CondominiumAlerts.Api/Endpoints/AuthModule.cs
@@ -91,6 +91,6 @@ public class AuthModule : ICarterModule
                 };
                 
                 return Results.Ok(response);
-            });
+            }).RequireAuthorization();
     }
 }
--- CondominiumAlerts.Backend/CondominiumAlerts.Features/Features/Users/Get/GetUserDataResponse.cs
+++ CondominiumAlerts.Backend/CondominiumAlerts.Features/Features/Users/Get/GetUserDataResponse.cs
@@ -2,4 +2,4 @@
 
 namespace CondominiumAlerts.Features.Features.Users;
 
-public record GetUserDataResponse(string Id, string Name, string LastName, string Email, string ProfilePictureUrl, Address? Address, DateTime CreatedAt);
+public record GetUserDataResponse(string Id, string Name, string Lastname, string Username, string Email, string ProfilePictureUrl, Address? Address, DateTime CreatedAt);
