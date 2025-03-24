8f889ec6b4ae7951122bec9e5812d52a65fcf2b0
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2025-03-24 19:38:10 -0400

    chore: Fix small orthographical error

--- CondominiumAlerts.Backend/CondominiumAlerts.Features/Features/Condominiums/Join/JoinCondominiumValidator.cs
+++ CondominiumAlerts.Backend/CondominiumAlerts.Features/Features/Condominiums/Join/JoinCondominiumValidator.cs
@@ -11,8 +11,8 @@ namespace CondominiumAlerts.Features.Features.Condominiums.Join
 
             RuleFor(jc => jc.CondominiumCode).NotEmpty().WithMessage("The code was not passed");
 
-            RuleFor(jc => jc.CondominiumCode).MinimumLength(11).WithMessage("The code is to short, it must be 11 characters long");
-            RuleFor(jc => jc.CondominiumCode).MaximumLength(11).WithMessage("The code is to long, it must be 11 characters long");
+            RuleFor(jc => jc.CondominiumCode).MinimumLength(11).WithMessage("The code is too short, it must be 11 characters long");
+            RuleFor(jc => jc.CondominiumCode).MaximumLength(11).WithMessage("The code is too long, it must be 11 characters long");
         }
     }
 }
