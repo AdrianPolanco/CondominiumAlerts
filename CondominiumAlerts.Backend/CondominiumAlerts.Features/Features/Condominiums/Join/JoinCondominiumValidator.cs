565e0e451f028a910d636204ecc9ef44262dfdf3
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2025-03-24 20:26:35 -0400

    fix: Fix code length validation

--- CondominiumAlerts.Backend/CondominiumAlerts.Features/Features/Condominiums/Join/JoinCondominiumValidator.cs
+++ CondominiumAlerts.Backend/CondominiumAlerts.Features/Features/Condominiums/Join/JoinCondominiumValidator.cs
@@ -10,9 +10,8 @@ namespace CondominiumAlerts.Features.Features.Condominiums.Join
             RuleFor(jc => jc.UserId).NotEmpty().WithMessage("The user was not especified");
 
             RuleFor(jc => jc.CondominiumCode).NotEmpty().WithMessage("The code was not passed");
-
-            RuleFor(jc => jc.CondominiumCode).MinimumLength(11).WithMessage("The code is too short, it must be 11 characters long");
-            RuleFor(jc => jc.CondominiumCode).MaximumLength(11).WithMessage("The code is too long, it must be 11 characters long");
+            RuleFor(jc => jc.CondominiumCode).MinimumLength(10).WithMessage("The code is too short, it must be 11 characters long");
+            RuleFor(jc => jc.CondominiumCode).MaximumLength(12).WithMessage("The code is too long, it must be 11 characters long");
         }
     }
 }
