5177a1889bb202b3effdaef3ea84d3acd4f107f4
Luis De Los Santos <dluisaneudy82@gmail.com>
2025-03-30 22:25:45 -0400

    creating the add message command

---
+++ CondominiumAlerts.Backend/CondominiumAlerts.Features/Features/Messages/Create/AddMessageCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using CondominiumAlerts.CrossCutting.CQRS.Interfaces.Handlers;
+using CondominiumAlerts.Domain.Aggregates.Entities;
+using CondominiumAlerts.Domain.Repositories;
+using LightResults;
+
+namespace CondominiumAlerts.Features.Features.Messages.Create
+{
+    public class AddMessageCommandHandler(IRepository<Message, Guid> messageRepository) : ICommandHandler<AddMessageCommand, Result<MessageDto>>
+    {
+        public async Task<Result<MessageDto>> Handle(AddMessageCommand request, CancellationToken cancellationToken)
+        {
+            var message = new Message()
+            {
+                CreatedAt = DateTime.UtcNow,
+                CondominiumId = request.CondominiumId,
+                Text = request.Text,
+                MediaUrl = request.MediaUrl,
+            };
+
+            var messageAdded = await messageRepository.CreateAsync(message, cancellationToken);
+            return Result.Ok(new MessageDto(
+                messageAdded.Id,
+                messageAdded.Text,  
+                messageAdded.MediaUrl,  
+                messageAdded.CondominiumId, 
+                messageAdded.ReceiverUserId,
+                messageAdded.MessageBeingRepliedToId, message.CreatedAt));
+        }
+    }
+}
---
+++ CondominiumAlerts.Backend/CondominiumAlerts.Features/Features/Messages/MessageDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CondominiumAlerts.Features.Features.Messages;
+
+public record MessageDto(
+    Guid Id,
+    string Text,
+    string? MediaUrl,
+    Guid? CondominiumId,
+    string? ReceiverUserId,
+    Guid? MessageBeingRepliedToId,
+    DateTime CreatedAt
+ );
+
