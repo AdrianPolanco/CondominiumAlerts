c20183e30575bf86dc3d0112f319d865f4e2f013
sylcr <sylcr@MALVO>
2025-04-07 19:24:57 -0400

    Posts sin imagenes

--- CondominiumAlerts.Backend/CondominiumAlerts.Features/Features/Posts/Show/GetPostsResponse.cs
+++ CondominiumAlerts.Backend/CondominiumAlerts.Features/Features/Posts/Show/GetPostsResponse.cs
@@ -5,7 +5,7 @@ namespace CondominiumAlerts.Features.Features.Posts.Get
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public Guid CondominiumId { get; set; }
