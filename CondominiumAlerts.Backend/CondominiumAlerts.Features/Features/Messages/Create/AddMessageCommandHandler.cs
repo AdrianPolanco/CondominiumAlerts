9aee2aec9ce1a2ee9bfd3e1b68c7b258914cf162
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2025-04-13 23:07:57 -0400

    Add notification for Condominum messages

--- CondominiumAlerts.Backend/CondominiumAlerts.Features/Features/Messages/Create/AddMessageCommandHandler.cs
+++ CondominiumAlerts.Backend/CondominiumAlerts.Features/Features/Messages/Create/AddMessageCommandHandler.cs
@@ -1,13 +1,16 @@
 ﻿using CloudinaryDotNet;
 using CondominiumAlerts.CrossCutting.CQRS.Interfaces.Handlers;
 using CondominiumAlerts.Domain.Aggregates.Entities;
+using CondominiumAlerts.Domain.Interfaces;
 using CondominiumAlerts.Domain.Repositories;
 using LightResults;
 
 namespace CondominiumAlerts.Features.Features.Messages.Create
 {
-    public class AddMessageCommandHandler(IRepository<Message, Guid> messageRepository, Cloudinary cloudinary) : ICommandHandler<AddMessageCommand, Result<MessageDto>>
+    public class AddMessageCommandHandler(IRepository<Message, Guid> messageRepository, Cloudinary cloudinary, INotificationService notificationService) : ICommandHandler<AddMessageCommand, Result<MessageDto>>
     {
+        private readonly INotificationService _notificationService = notificationService;
+
         public async Task<Result<MessageDto>> Handle(AddMessageCommand request, CancellationToken cancellationToken)
         {
             var message = new Message()
@@ -26,6 +29,19 @@ namespace CondominiumAlerts.Features.Features.Messages.Create
                 messageWithCreatorUser.CreatorUser.Lastname,
                 messageWithCreatorUser.CreatorUser.ProfilePictureUrl,
                 messageWithCreatorUser.CreatorUser.Username);
+            if (message.CondominiumId.HasValue)
+            {
+                await _notificationService.Notify(new Notification
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "New Message",
+                    Description = $"New message from {messageWithCreatorUser.CreatorUser?.Name}",
+                    CondominiumId = message.CondominiumId.Value,
+                    LevelOfPriorityId = 
+                    CreatedAt = DateTime.UtcNow,
+                    ReceiverUserId = request.ReceiverUserId // Only notify specific user if it's a direct message
+                }, message.CondominiumId.Value.ToString(), cancellationToken);
+            }
 
             return Result.Ok(new MessageDto(
                 messageWithCreatorUser!.Id,
