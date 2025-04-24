25c17b9dcefc5ebc3f4bd310c1b63882609c481e
sylcr <sylcr@MALVO>
2025-04-24 15:16:03 -0400

    feat: Edicion de comentarios

---
+++ CondominiumAlerts.Backend/CondominiumAlerts.Features/Features/Comment/Update/UpdateCommentResponse.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CondominiumAlerts.Features.Features.Comment.Update
+{
+    public class UpdateCommentResponse
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; } = string.Empty!;
+        public string ImageUrl { get; set; }
+    }
+}
