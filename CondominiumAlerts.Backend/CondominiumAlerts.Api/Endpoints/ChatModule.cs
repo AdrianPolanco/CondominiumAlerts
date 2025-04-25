b4eaa9cb595f27b7d3dab996bba9313527c21fc3
Adrian Polanco <adferrer0611@gmail.com>
2025-04-25 16:17:06 -0400

    fix(summaries): Agregar datos significativos del usuario creador el mensaje al prompt (ej: Nombre de usuario y nombre completo) para evitar que haga referencia a los ids en el resumen

--- CondominiumAlerts.Backend/CondominiumAlerts.Api/Endpoints/ChatModule.cs
+++ CondominiumAlerts.Backend/CondominiumAlerts.Api/Endpoints/ChatModule.cs
@@ -1,5 +1,11 @@
-﻿using Carter;
+﻿using System.Net;
+using System.Security.Claims;
+using Carter;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 using CondominiumAlerts.Api.Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Polly;
 
 namespace CondominiumAlerts.Api.Endpoints
 {
@@ -8,6 +14,42 @@ namespace CondominiumAlerts.Api.Endpoints
         public void AddRoutes(IEndpointRouteBuilder app)
         {
             app.MapHub<ChatHub>("/hubs/chat");
+
+            app.MapPost("/chat/image", async (
+                [FromServices] IAsyncPolicy retryPolicy,
+                [FromServices] Cloudinary cloudinary,
+                ClaimsPrincipal claims,
+                [FromForm]IFormFile file) =>
+            {
+                var currentUserId = claims.FindFirst("user_id")?.Value;
+
+                if (string.IsNullOrWhiteSpace(currentUserId.Trim())) return Results.Unauthorized();
+                    
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, file.OpenReadStream()),
+                };
+
+                var uploadResult =
+                    await retryPolicy.ExecuteAsync(async () => await cloudinary.UploadAsync(uploadParams));
+
+                var isSuccess = uploadResult.StatusCode == HttpStatusCode.OK;
+
+                var response = new
+                {
+                    isSuccess = isSuccess,
+                    Data = new
+                    {
+                        ImageUrl = isSuccess ? uploadResult.SecureUrl.ToString() : null
+                    }
+                };
+
+                if (!isSuccess) return Results.InternalServerError(response);
+
+                return Results.Ok(response);
+            })
+                .DisableAntiforgery()
+                .RequireAuthorization();
         }
     }
 }
