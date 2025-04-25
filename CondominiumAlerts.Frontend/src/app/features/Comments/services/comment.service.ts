d2863758d1612ac85fcc895a17c71999c6afe2c6
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2025-04-25 11:16:48 -0400

    Fix imports

--- CondominiumAlerts.Frontend/src/app/features/Comments/services/comment.service.ts
+++ CondominiumAlerts.Frontend/src/app/features/Comments/services/comment.service.ts
@@ -9,9 +9,9 @@ import { getCommentByPostCommand } from '../../Comments/models/getCommentByPost.
 import { getCommentByPostResponse } from '../../Comments/models/getCommentByPost.Reponse'
 import { UpdateCommentResponse } from '../../Comments/models/updateComment.Response'
 import { UpdateCommentCommand } from '../../Comments/models/updateComment.Command'
-import { DeleteCommentCommand } from '../../Comments/models/deletecomment.Command'
 import { DeleteCommentResponse } from '../../Comments/models/deleteComment.Response'
 import { AuthService } from '../../../core/auth/services/auth.service';
+import { DeleteCommentCommand } from '../models/deleteComment.Command';
 
 @Injectable({
   providedIn: 'root',
